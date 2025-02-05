0ebc188330c3eb23e7e2789fa7dfd2731709d341
TruongHung02 <truongviethung2682002@gmail.com>
2025-02-05 08:20:19 +0000

    update config file

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,13 @@
 export const config = {
+  //profile config
   USER_AGENT:
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-  EXECUTABLE_PATH: '/snap/bin/chromium',
   script: 'keyboard.genlogin.json',
+
+  //ubuntu chromnium path
+  EXECUTABLE_PATH: '/snap/bin/chromium',
+
+  //browser config
   headless: false,
+  window_size: '1024,768',
 }
--- src/helper/create-browser.ts
+++ src/helper/create-browser.ts
@@ -6,7 +6,7 @@ export default async function createBrowser(proxyString?: string): Promise<Brows
     `--user-agent=${config.USER_AGENT}`,
     '--enable-cookies',
     '--enable-javascript',
-    '--window-size=1920,1080',
+    `--window-size=${config.window_size}`,
     'accept=text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     '--no-sandbox',
     '--disable-dev-shm-usage',
